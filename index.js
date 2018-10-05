422dc74bbe02ab2b2cc72ff360623e8d852d6a6d
Tom Roggero <tomas.roggero@rga.com>
2018-10-05 13:44:29 -0400

    CORS enabled

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ exports.handler = async (event) => {
     return {
         statusCode: 200,
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
         },
         body: JSON.stringify({ tweet });
     };
